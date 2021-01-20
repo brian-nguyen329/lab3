4feaeebad47cdd29eabc1d91d377dc1f0378077c
brian-nguyen329 <brnguyen@ucsd.edu>
2021-01-19 23:54:06 -0800

    finished lab?

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,39 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+
+		// added to change the button
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").addClass("active");
+
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+
+}
+
+function projectClick(e) {
+    // prevent the page from reloading
+    e.preventDefault();
+    // In an event handler, $(this) refers to    
+	// the object that triggered the event  
+	
+	// $(this).css("background-color", "#7fff00"); (FROM LAB 2)
+
+
+	// First part of lab 3 -- we want to replace not append!
+	/* var containingProject = $(this).closest(".project");
+    containingProject.append("<div class='project-description'><p>Description of the project.</p></div>"); */
+
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+		// Before I changed to control content visibility
+		// description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+		$(description).fadeToggle();
+    }
 }
